0ae526bd2b647df95e0fb7e53a7b4b9ac1779852
Nicole Reed <nickreed033@gmail.com>
2022-06-24 17:56:02 +0300

    add client modal and logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
 
 // To get rid of cache warning
 const cache = new InMemoryCache({
@@ -33,6 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
 
         </div>
--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,5 +1,16 @@
 import { gql } from '@apollo/client';
 
+const ADD_CLIENT = gql`
+    mutation addClient($name: String!, $email: String!, $phone: String!) {
+        addClient(name: $name, email: $email, phone: $phone) {
+            id
+            name
+            email
+            phone
+        }
+    }
+`;
+
 const DELETE_CLIENT = gql`
     mutation deleteClient($id: ID!) {
         deleteClient(id: $id) {
@@ -9,6 +20,6 @@ const DELETE_CLIENT = gql`
             phone
         }
     }
-`
+`;
 
-export { DELETE_CLIENT };
+export { ADD_CLIENT, DELETE_CLIENT };
